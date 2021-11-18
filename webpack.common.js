d983cb0a2f6096f6c67b7c98a732aedac401cf0f
tmiyachi <t.miyachi@hotmail.co.jp>
2021-11-18 09:05:26 +0900

    設定ファイルのオプション誤りを修正

--- webpack.common.js
+++ webpack.common.js
@@ -38,7 +38,7 @@ module.exports = {
             loader: 'css-loader',
             options: {
               url: false,
-              importLoaders: 2,
+              importLoaders: 1,
             },
           },
           {
