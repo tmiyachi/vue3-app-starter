ac3bb4b2c2be5e111ff0f59dad9010e8bae35315
tmiyachi <t.miyachi@hotmail.co.jp>
2021-12-11 19:03:27 +0900

    sass-loader の implementation 指定を省略
    package.jsonの記述があればDart Sassが採用されるため

--- webpack.common.js
+++ webpack.common.js
@@ -43,9 +43,6 @@ module.exports = {
           },
           {
             loader: 'sass-loader',
-            options: {
-              implementation: require('sass'),
-            },
           },
         ],
       },
