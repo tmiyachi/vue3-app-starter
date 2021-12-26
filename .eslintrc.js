d829a85daa1a8765125de5433d8ec83e308c2f40
tmiyachi <t.miyachi@hotmail.co.jp>
2021-12-26 20:23:43 +0900

    ESlintの重複する設定を削除

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,9 @@
+/* eslint-env node */
 module.exports = {
   env: {
     browser: true,
-    es6: true,
-    node: true,
+    es2020: true,
   },
   extends: ['eslint:recommended', 'plugin:vue/vue3-recommended', 'prettier'],
-  parser: 'vue-eslint-parser',
-  parserOptions: {
-    sourceType: 'module',
-    ecmaVersion: 2020,
-  },
   rules: {},
 };
