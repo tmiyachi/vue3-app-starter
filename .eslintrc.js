97097b71085b6fd16ea7f7bb5c1e9562f5c7f943
tmiyachi <t.miyachi@hotmail.co.jp>
2022-06-28 14:16:54 +0900

    lintルールにカンマ演算子の使用禁止を追加

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,5 +8,6 @@ module.exports = {
   extends: ['eslint:recommended', 'plugin:vue/vue3-recommended', 'prettier'],
   rules: {
     'consistent-return': 'error',
+    'no-sequences': 'error',
   },
 };
