e1f9aabf20bd4ea61bf7e50f9137412724e5f64c
tmiyachi <t.miyachi@hotmail.co.jp>
2024-07-20 10:08:35 +0900

    ESlintをflat configへ移行

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,10 +1,10 @@
-module.exports = {
+/** @type {import('stylelint').Config} */
+export default {
+  ignoreFiles: ['dist/**/*'],
   extends: [
     'stylelint-config-recess-order',
     'stylelint-config-recommended-vue/scss',
   ],
-  // additional configuration to validate scss with vscode-stylelint extension
-  // https://github.com/stylelint/vscode-stylelint#%EF%B8%8F-only-css-and-postcss-are-validated-by-default
   overrides: [
     {
       files: ['**/*.scss'],
